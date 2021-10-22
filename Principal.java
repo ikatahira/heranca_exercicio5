9c9d55ea95c89b73b9e5be01b19318184b9a5b90
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-10-22 00:58:21 +0000

    Finalizando o exercício 5

--- Principal.java
+++ Principal.java
@@ -7,5 +7,12 @@ public class Principal {
     n.setPrecoAdicional(200);
     System.out.println("Endereçõ: "+n.getEndereco()+" Preço: "+ n.getPreco()+" Preço adicional"+ n.getPrecoAdicional());
 
+    Velho v=new Velho();
+    v.setEndereco("Avenida Hassib Mofarrej, Oriente");
+    v.setPreco(100000);
+    v.setDescontoPreco(1000);
+    System.out.println("Endereço: "+v.getEndereco()+" Preço: "+v.getPreco()+" Desconto:" +v.getDescontoPreco());
+
+
   }  
 }
